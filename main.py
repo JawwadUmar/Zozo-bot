f62df74a6a6d82bb4e8ed11eda954c77ce3cb835
Jawwad Umar <jawwadumar@Jawwads-MacBook-Pro.local>
2026-04-30 14:02:25 +0530

    refactoring

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 import asyncio
 import random
 from playwright.async_api import async_playwright
+from app.config.config import EMAIL, PASSWORD
 
 async def human_typing(element, text):
     for char in text:
         await element.type(char, delay=random.randint(40, 120))
         if random.random() < 0.07:
             await asyncio.sleep(random.uniform(0.1, 0.3))
+
+async def human_mouse_move(page):
+    await page.mouse.move(
+        random.randint(100, 800),
+        random.randint(100, 600),
+        steps=15
+    )
+async def human_delay(min_sec=2, max_sec=5):
+    await asyncio.sleep(random.uniform(min_sec, max_sec))
 
 async def run():
     async with async_playwright() as p:
@@ -36,16 +46,19 @@ async def run():
         await page.wait_for_load_state("domcontentloaded")
 
         # Better selectors
-        username = page.get_by_label("Email or phone", exact=True).locator("visible=true")
-        password = page.get_by_label("Password", exact=True).locator("visible=true")
+        username = page.get_by_label("Email or phone", exact=True).locator("visible=true").first
+        password = page.get_by_label("Password", exact=True).locator("visible=true").first
 
         await username.click()
-        await human_typing(username, "your_email")
+        await human_typing(username, EMAIL)
 
         await password.click()
-        await human_typing(password, "your_password")
+        await human_typing(password, PASSWORD)
 
-        await page.locator("button[type='submit']").click()
-        # await browser.close()
+        submitButton = page.get_by_role("button", name="Sign in", exact=True).locator("visible=true").first
+        await submitButton.click()
+        await human_delay(6, 10)
+
+        await browser.close()
 
 asyncio.run(run())
